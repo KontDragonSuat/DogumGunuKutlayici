448086d18ab5c05194a5cc157e0e89598d7fd11f
KontDragonSuat <122736828+KontDragonSuat@users.noreply.github.com>
2023-04-16 18:05:44 +0200

    Update postaci.py

--- postaci.py
+++ postaci.py
@@ -5,6 +5,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from hesap import account, password
 
 def zamangeldi():
